f6f1859a23544c965b5da0c726b1bc45fd506273
YukinoKyoU <ywx505246@gmail.com>
2021-03-10 05:34:12 +0800

    Tweak the frequency to 4

--- 4.Perlin Noise/texture.hpp
+++ 4.Perlin Noise/texture.hpp
@@ -57,13 +57,16 @@ class checker_texture : public texture {
 class noise_texture : public texture {
     public:
         noise_texture(){}
+        //增加一个频率参数
+        noise_texture(double sc) : scale(sc) {}
 
         virtual color value(double u, double v, const point3& p) const override{
-            return color(1, 1, 1) * noise.noise(p);
+            return color(1, 1, 1) * noise.noise(scale * p);
         }
 
     private:
         perlin noise;
+        double scale;
 };
 
 #endif
