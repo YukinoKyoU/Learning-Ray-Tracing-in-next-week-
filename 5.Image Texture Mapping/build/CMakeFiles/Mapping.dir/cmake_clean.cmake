7f6189dd5cca1a13f663e32a2e3c914147803d29
YukinoKyoU <ywx505246@gmail.com>
2021-03-11 00:45:03 +0800

    Image Texture Mapping

---
+++ 5.Image Texture Mapping/build/CMakeFiles/Mapping.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mapping.dir/main.cpp.o"
+  "Mapping.pdb"
+  "Mapping"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
